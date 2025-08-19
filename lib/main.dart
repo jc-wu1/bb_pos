15aa09805ed235f262ad50ab2d75f806fc2ab7af
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2025-08-19 17:29:41 +0700

    remove isar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'dart:io';
 
-import 'package:bb_pos/features/receipt/receipt.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:isar/isar.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'jcs_pos_app.dart';
 
@@ -13,14 +9,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (Platform.isAndroid) {
-    // Workaround for https://github.com/flutter/flutter/issues/35162
     await FlutterDisplayMode.setHighRefreshRate();
   }
-
-  late final Isar isar;
-
-  final dir = await getApplicationDocumentsDirectory();
-  isar = await Isar.open([ReceiptSchema, ProductSchema], directory: dir.path);
 
   runApp(const JcsPosApp());
 }
