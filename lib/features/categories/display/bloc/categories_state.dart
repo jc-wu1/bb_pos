90cb6045752c06cd3567e1bbd27c5663d2936930
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2025-08-20 15:14:33 +0700

    WIP categories feature

--- lib/features/categories/display/bloc/categories_state.dart
+++ lib/features/categories/display/bloc/categories_state.dart
@@ -2,9 +2,20 @@ part of 'categories_bloc.dart';
 
 sealed class CategoriesState extends Equatable {
   const CategoriesState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class CategoriesInitial extends CategoriesState {}
+
+final class CategoriesLoadInProgress extends CategoriesState {}
+
+final class CategoriesLoadComplete extends CategoriesState {
+  final List<CategoryItem> categories;
+
+  const CategoriesLoadComplete({required this.categories});
+
+  @override
+  List<Object> get props => [categories];
+}
