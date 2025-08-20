bba0c4796459e9788b21f7533da20870fef22a14
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2025-08-20 15:14:12 +0700

    WIP categories feature

--- lib/features/categories/display/bloc/categories_bloc.dart
+++ lib/features/categories/display/bloc/categories_bloc.dart
@@ -1,13 +1,38 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
+import '../../data/model/category.dart';
+import '../../domain/usecases/categories_usecase.dart';
 
 part 'categories_event.dart';
 part 'categories_state.dart';
 
 class CategoriesBloc extends Bloc<CategoriesEvent, CategoriesState> {
-  CategoriesBloc() : super(CategoriesInitial()) {
-    on<CategoriesEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final CategoriesUsecase _usecase;
+  CategoriesBloc({required CategoriesUsecase usecase})
+    : _usecase = usecase,
+      super(CategoriesInitial()) {
+    on<CategoriesFetched>(_onCategoriesFetched);
+    on<CategoryInserted>(_onCategoryInserted);
+  }
+
+  Future<void> _onCategoriesFetched(
+    CategoriesFetched event,
+    Emitter<CategoriesState> emit,
+  ) async {
+    emit(CategoriesLoadInProgress());
+    final result = await _usecase.getCategories();
+    emit(CategoriesLoadComplete(categories: result));
+  }
+
+  Future<void> _onCategoryInserted(
+    CategoryInserted event,
+    Emitter<CategoriesState> emit,
+  ) async {
+    emit(CategoriesLoadInProgress());
+    await _usecase.insertCategory(event.categoryItem);
+    add(const CategoriesFetched());
   }
 }
