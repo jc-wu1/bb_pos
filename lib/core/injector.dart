cd39c5162cf51d117a8ff07d3348d02fa61d0dda
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2025-08-20 14:33:20 +0700

    register menus and categories  dependencies

--- lib/core/injector.dart
+++ lib/core/injector.dart
@@ -1,6 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../features/categories/data/data_sources/categories_local_data_source.dart';
+import '../features/categories/data/repositories/categories_repository_impl.dart';
+import '../features/categories/domain/repositories/categories_repository.dart';
+import '../features/categories/domain/usecases/categories_usecase.dart';
+import '../features/menus/data/data_sources/menus_local_data_source.dart';
+import '../features/menus/data/repositories/menus_repository_impl.dart';
+import '../features/menus/domain/repositories/menus_repository.dart';
+import '../features/menus/domain/usecases/menus_usecase.dart';
 import 'db_services.dart';
 
 final sl = GetIt.instance;
@@ -8,4 +16,35 @@ final sl = GetIt.instance;
 Future<void> initDbService() async {
   final DbServices dbServices = DbServices();
   sl.registerSingletonAsync<Database>(() async => await dbServices.database());
+}
+
+Future<void> initCoreDependencies() async {
+  /// Data source
+  sl.registerLazySingleton<CategoriesLocalDataSource>(
+    () => CategoriesLocalDataSourceImpl(db: sl<Database>()),
+  );
+  sl.registerLazySingleton<MenusLocalDataSource>(
+    () => MenusLocalDataSourceImpl(db: sl<Database>()),
+  );
+  ////
+
+  /// Repositories
+  sl.registerLazySingleton<CategoriesRepository>(
+    () => CategoriesRepositoryImpl(
+      localDataSource: sl<CategoriesLocalDataSource>(),
+    ),
+  );
+  sl.registerLazySingleton<MenusRepository>(
+    () => MenusRepositoryImpl(localDataSource: sl<MenusLocalDataSource>()),
+  );
+  ////
+
+  /// Usecase
+  sl.registerLazySingleton<CategoriesUsecase>(
+    () => CategoriesUsecase(repository: sl<CategoriesRepository>()),
+  );
+  sl.registerLazySingleton<MenusUsecase>(
+    () => MenusUsecase(repository: sl<MenusRepository>()),
+  );
+  ////
 }
