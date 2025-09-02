51d4385bbd851bb23f79184b42e6f07211d24e74
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2025-09-02 17:29:25 +0700

    remove unused static method

--- lib/features/supabase/domain/usecases/supabase_usecase.dart
+++ lib/features/supabase/domain/usecases/supabase_usecase.dart
@@ -7,14 +7,6 @@ class SupabaseUsecase {
 
   SupabaseUsecase({required this.repository});
 
-  static File createFile(String path) {
-    final file = File(path);
-    if (!file.existsSync()) {
-      file.createSync(recursive: true);
-    }
-    return file;
-  }
-
   Future<String> uploadImage(File imageFile) async {
     try {
       return await repository.uploadImage(imageFile);
