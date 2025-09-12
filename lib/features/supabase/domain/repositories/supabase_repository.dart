04414dc13a9f8bc4c769601e7c3434dd966ff6bd
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2025-09-12 16:04:02 +0700

    refactor: remove Supabase-related files

--- lib/features/supabase/domain/repositories/supabase_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:firebase_storage/firebase_storage.dart';
-
-abstract class SupabaseRepository {
-  Future<TaskSnapshot> uploadImage(File imageFile);
-  Future<void> deleteImage(String path);
-}
