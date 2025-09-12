cd96d5d82d3ec51749d1c76b899f879ccd3d3099
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2025-09-12 16:04:25 +0700

    refactor: remove Firebase initialization code and related imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
 import 'dart:io';
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 
-import 'firebase_options.dart';
 import 'jcs_pos_app.dart';
-
-late final FirebaseApp app;
-late final FirebaseAuth auth;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  app = await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  auth = FirebaseAuth.instanceFor(app: app);
 
   if (Platform.isAndroid) {
     await FlutterDisplayMode.setHighRefreshRate();
